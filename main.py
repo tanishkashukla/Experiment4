d2c661ee2931ba683a12ff57de232ab7b38ff18c
tanishka shukla <tanishkashukla16@gmail.com>
2026-02-10 20:35:54 +0530

    Fix typo in multiplication result print statement

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import multiplication as m
 
 print("Addition Result =", a.add(5,6))
 print("Subtraction Result =",s.sub(5,6))
-print("Multiplication Reult =", m.mul(5,6))
+print("Multiplication Result =", m.mul(5,6))
 
 #Call Division method here
